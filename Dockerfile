17c04674a43535373d8a6627cecf8193e3460db9
Matthew C. Jones, CPA, CISA, OSCP, CCFE <mjones@isaudits.com>
2019-03-14 11:27:00 -0400

    tweak defaults and add custom SSL cert to image

--- Dockerfile
+++ Dockerfile
@@ -9,6 +9,7 @@ RUN apk update && \
     git clone --depth=1 --recursive https://github.com/beefproject/beef/ /home/beef && \
     cd /home/beef && \
     bundle install --without test development && \
+    ./generate-certificate && \
     rm -rf /home/beef/.git && \
     apk del git build-base automake && \
     rm -rf /var/cache/apk/*
@@ -18,20 +19,21 @@ WORKDIR /home/beef/
 ENV BEEF_WAITTIME="0" \
     BEEF_USER="beefuser" \
     BEEF_PASSWORD="beefpass" \
-    BEEF_PUBLIC_IP="fqdn.domain.com" \
-    BEEF_PUBLIC_PORT="443" \
-    BEEF_MSF_ENABLE="true" \
-    MSF_RPC_HOST="0.0.0.0" \
-    MSF_RPC_PORT="55553" \
-    MSF_RPC_USER="msfuser" \
-    MSF_RPC_PASS="msfpass" \
-    MSF_RPC_SSL="true" \
-    MSF_SSL_VERIFY="false" \
-    MSF_CALLBACK_HOST="fqdn.domain.com" \
-    BEEF_SE_ENABLE="true"  \
-    SE_POSH_HOST="fqdn.domain.com" \
-    SE_POSH_PORT="4343" \
-    BEEF_PHISHINGFRENZY_ENABLE="true"
+    BEEF_SSL="true" \
+    #BEEF_PUBLIC_IP="fqdn.domain.com" \
+    #BEEF_PUBLIC_PORT="443" \
+    BEEF_MSF_ENABLE="false" \
+    #MSF_RPC_HOST="0.0.0.0" \
+    #MSF_RPC_PORT="55553" \
+    #MSF_RPC_USER="msfuser" \
+    #MSF_RPC_PASS="msfpass" \
+    #MSF_RPC_SSL="true" \
+    #MSF_SSL_VERIFY="false" \
+    #MSF_CALLBACK_HOST="fqdn.domain.com" \
+    BEEF_SE_ENABLE="false"  \
+    #SE_POSH_HOST="fqdn.domain.com" \
+    #SE_POSH_PORT="4343" \
+    BEEF_PHISHINGFRENZY_ENABLE="false"
 
 #NOTE - have to chmod 755 entrypoint script on source filesystem or it will not be executable inside container!
 COPY entrypoint.sh /home/beef/entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -2,10 +2,18 @@
 
 sed -i "s/passwd: \"beef\"/passwd: \"$BEEF_PASSWORD\"/" config.yaml
 sed -i "s/user:   \"beef\"/user: \"$BEEF_USER\"/" config.yaml
-sed -i "s/#public: \"\"/public: \"$BEEF_PUBLIC_IP\"/" config.yaml
-sed -i "s/#public_port: \"\"/public_port: \"$BEEF_PUBLIC_PORT\"/" config.yaml
 
-if [ $BEEF_MSF_ENABLE="true" ]; then
+if [ "$BEEF_SSL" == "true" ]; then
+    ./generate-certificate
+    sed -i "{N;s/https:\n            enable: false/https:\n            enable: true/}" config.yaml
+fi
+
+if [ -n "$BEEF_PUBLIC_IP" ]; then
+    sed -i "s/#public: \"\"/public: \"$BEEF_PUBLIC_IP\"/" config.yaml
+    sed -i "s/#public_port: \"\"/public_port: \"$BEEF_PUBLIC_PORT\"/" config.yaml
+fi
+
+if [ "$BEEF_MSF_ENABLE" == "true" ]; then
     sed -i "{N;s/metasploit:\n            enable: false/metasploit:\n            enable: $BEEF_MSF_ENABLE/}" config.yaml
     # add leading space so we dont mangle callback host too!
     sed -i "s/ host: \"127.0.0.1\"/ host: \"$MSF_RPC_HOST\"/" extensions/metasploit/config.yaml
@@ -18,14 +26,14 @@ if [ $BEEF_MSF_ENABLE="true" ]; then
 fi
 
 
-if [ $BEEF_SE_ENABLE="true" ]; then
+if [ "$BEEF_SE_ENABLE" == "true" ]; then
     #Probably not necessary - enabled by default in config file...
     sed -i "{N;s/social_engineering:\n            enable: false/social_engineering:\n            enable: $BEEF_SE_ENABLE/}" config.yaml
     sed -i "s/msf_reverse_handler_host: \"172.16.45.1\"/msf_reverse_handler_host: \"$SE_POSH_HOST\"/" extensions/social_engineering/config.yaml
     sed -i "s/msf_reverse_handler_port: \"443\"/msf_reverse_handler_port: \"$SE_POSH_PORT\"/" extensions/social_engineering/config.yaml
 fi
 
-if [ $BEEF_PHISHINGFRENZY_ENABLE="true" ]; then
+if [ "$BEEF_PHISHINGFRENZY_ENABLE" == "true" ]; then
     sed -i "{N;s/phishing_frenzy:\n            enable: false/phishing_frenzy:\n            enable: $BEEF_PHISHINGFRENZY_ENABLE/}" config.yaml
 fi
 
